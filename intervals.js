8b55ddfe2d110b2bb134bed09ec12f417b9460d9
Ozzie3000 <ozzieboy19@hotmail.com>
2014-09-29 22:41:49 -0400

    testing setIntervals

--- intervals.js
+++ intervals.js
@@ -1,6 +1,15 @@
-
-
-
+/*
 setInterval(function(){
 	console.log(new Date());
-} 2000);
+}, 2000);
+*/
+
+//in order to stop it you give it a function_name
+var myinterVal = setInterval(function(){
+	console.log(new Date());
+}, 2000);
+
+
+// stops setInterval
+//clearInterval(function_name);
+clearInterval(myinterVal);
